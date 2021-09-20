0907dbbdc91cc39ae51ca3b83c68b308d2b6ed85
Jesper Søndergaard Jensen <jesper.sondergaard.jensen@gmail.com>
2021-09-20 12:22:34 +0200

    use " and not ' in code

--- src/tests/scriptHelper.test.ts
+++ src/tests/scriptHelper.test.ts
@@ -1,7 +1,7 @@
 import { scriptHelper } from "../helpers/scriptHelper";
 
 test("test action", () => {
-  let result = `(function () { return console.log('works'); })(consent);`;
+  let result = `(function () { return console.log("works"); })(consent);`;
   let test = `__afterConsent__(consent);`;
   let merged = scriptHelper(
     "UA-XXXXXXXX-Y",
